92965ec28bdacd335ef330aac3720bee441f8f33
sohailIlyas1996 <51970582+sohailIlyas1996@users.noreply.github.com>
2024-01-02 23:37:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const studentModel=require('./student');
+
+
+app.get('/allstudents',async (req, res) => {
+    try {
+      const allStudents = await studentModel.find({});
+    // Create a new PDF document
+    
+  
+
+  
+    
+      // res.render('studentData', { students: allStudents}); // Render 'index.ejs' and pass fetched data
+    res.send(allStudents);
+    
+    
+    } catch (err) {
+      console.error('Error fetching student data:', err);
+      res.status(500).send('Error fetching student data');
+    }
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
